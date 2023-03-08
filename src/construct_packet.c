35af91d7897544bd0439742b4f5d631b6cdeffde
Colin Lam <colin.lph.lam@gmail.com>
2023-03-08 10:22:16 -0800

    implemented create_channel_dispatch

--- src/construct_packet.c
+++ src/construct_packet.c
@@ -20,6 +20,8 @@
 #define SIZE_SIZE 16
 #define BASE_HEADER_SIZE 32
 
+#define DELIMITER "\3"
+
 enum types {
     TYPE_CREATE = 1,
     TYPE_READ,
@@ -43,6 +45,7 @@ struct base_packet {
 };
 
 void create_user_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *login_token, char *display_name, char *password);
+void create_channel_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *channel_name, char *user_display_name, bool is_public);
 size_t serialize_packet(struct dc_env *env, struct dc_error *err, const struct base_packet *packet, uint8_t *serialized_packet);
 int send_packet(char* packet, char* server_ip, int server_port);
 
@@ -73,11 +76,56 @@ void create_user_dispatch(struct dc_env *env, struct dc_error *err, struct base_
         fprintf(stderr, "ERROR: %s \n", dc_error_get_message(err)); //NOLINT(cert-err33-c)
     }
     strcat(body, login_token);
-    strcat(body, "\3");
+    strcat(body, DELIMITER);
     strcat(body, display_name);
-    strcat(body, "\3");
+    strcat(body, DELIMITER);
     strcat(body, password);
-    strcat(body, "\3");
+    strcat(body, DELIMITER);
+
+    //duplicate body into packet->body
+    packet->body = (uint8_t*) strdup(body);
+
+    free(body);
+}
+
+void create_channel_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *channel_name, char *user_display_name, bool is_public)
+{
+    size_t size_of_body;
+    char *body;
+
+    packet = dc_malloc(env, err, sizeof(struct base_packet));
+    if(dc_error_has_error(err))
+    {
+        fprintf(stderr, "ERROR: %s \n", dc_error_get_message(err)); //NOLINT(cert-err33-c)
+    }
+    packet->version = CURRENT_VERSION;
+    packet->type = TYPE_CREATE;
+    packet->object = OBJECT_CHANNEL;
+
+    size_of_body = strlen(channel_name)+1;
+    size_of_body += strlen(user_display_name)+1;
+    size_of_body += 1+1;
+    packet->size = size_of_body;
+
+    //copy each field to the body
+    body = dc_malloc(env, err, size_of_body*sizeof(char));
+    if(dc_error_has_error(err))
+    {
+        fprintf(stderr, "ERROR: %s \n", dc_error_get_message(err)); //NOLINT(cert-err33-c)
+    }
+    strcat(body, channel_name);
+    strcat(body, DELIMITER);
+    strcat(body, user_display_name);
+    strcat(body, DELIMITER);
+    if(is_public)
+    {
+        strcat(body, "1");
+    }
+    else
+    {
+        strcat(body, "0");
+    }
+    strcat(body, DELIMITER);
 
     //duplicate body into packet->body
     packet->body = (uint8_t*) strdup(body);
