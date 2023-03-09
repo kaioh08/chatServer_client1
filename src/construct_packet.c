15cd536bc63af49d92c68cc6aa4cbb4d18eeb70b
Colin Lam <colin.lph.lam@gmail.com>
2023-03-09 12:30:42 -0800

    fixed serialize_header

--- src/construct_packet.c
+++ src/construct_packet.c
@@ -7,14 +7,21 @@
 #include <dc_error/error.h>
 #include <dc_env/env.h>
 #include <dc_c/dc_stdlib.h>
+#include <dc_posix/dc_string.h>
 
 #define CURRENT_VERSION 1
 
-#define VERSION_SIZE 4
-#define TYPE_SIZE 4
-#define OBJECT_SIZE 8
-#define SIZE_SIZE 16
-#define BASE_HEADER_SIZE 32
+#define VERSION_BITS 4
+#define TYPE_BITS 4
+#define OBJECT_BITS 8
+#define SIZE_BITS 16
+#define BASE_HEADER_BYTES 4
+
+#define VERSION_SHIFT 28
+#define TYPE_SHIFT 24
+#define OBJECT_SHIFT 16
+
+#define RESPONSE_SIZE 1024
 
 #define DELIMITER "\3"
 
@@ -40,12 +47,16 @@ struct base_packet {
     uint8_t *body;
 };
 
-void create_user_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *login_token, char *display_name, char *password);
-void create_channel_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *channel_name, char *user_display_name, bool is_public);
-size_t serialize_packet(struct dc_env *env, struct dc_error *err, const struct base_packet *packet, uint8_t *serialized_packet);
-int send_packet(char* packet, char* server_ip, int server_port);
+void create_user_dispatch(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *login_token, char *display_name, char *password);
+void create_channel_dispatch(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *channel_name, char *user_display_name, bool is_public);
+size_t serialize_packet(const struct dc_env *env, struct dc_error *err, const struct base_packet *packet, uint8_t *serialized_packet);
+void deserialize_packet(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, uint8_t *received_packet, ssize_t rbytes);
+int send_packet(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, char* server_ip, int server_port);
+ssize_t write_fully(int fd, const void *buffer, size_t len);
+ssize_t read_fully(int fd, void *buffer, size_t len);
 
-void create_user_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *login_token, char *display_name, char *password)
+
+void create_user_dispatch(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *login_token, char *display_name, char *password)
 {
     size_t size_of_body;
     char *body;
@@ -84,7 +95,7 @@ void create_user_dispatch(struct dc_env *env, struct dc_error *err, struct base_
     free(body);
 }
 
-void create_channel_dispatch(struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *channel_name, char *user_display_name, bool is_public)
+void create_channel_dispatch(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, char *channel_name, char *user_display_name, bool is_public)
 {
     size_t size_of_body;
     char *body;
@@ -129,13 +140,13 @@ void create_channel_dispatch(struct dc_env *env, struct dc_error *err, struct ba
     free(body);
 }
 
-size_t serialize_packet(struct dc_env *env, struct dc_error *err, const struct base_packet *packet, uint8_t *serialized_packet)
+size_t serialize_packet(const struct dc_env *env, struct dc_error *err, const struct base_packet *packet, uint8_t *serialized_packet)
 {
     size_t packet_size;
     size_t current_pos;
     uint16_t temp;
 
-    packet_size = BASE_HEADER_SIZE;
+    packet_size = BASE_HEADER_BYTES;
     packet_size += packet->size;
     temp = htons(packet->size);
 
@@ -146,21 +157,37 @@ size_t serialize_packet(struct dc_env *env, struct dc_error *err, const struct b
     {
         return 0;
     }
-    memcpy(&serialized_packet[current_pos], &packet->version, VERSION_SIZE);
-    current_pos += VERSION_SIZE;
-    memcpy(&serialized_packet[current_pos], &packet->type, TYPE_SIZE);
-    current_pos += TYPE_SIZE;
-    memcpy(&serialized_packet[current_pos], &packet->object, OBJECT_SIZE);
-    current_pos += OBJECT_SIZE;
-    memcpy(&serialized_packet[current_pos], &temp, SIZE_SIZE);
-    current_pos += SIZE_SIZE;
+
+    uint32_t header = packet->version << VERSION_SHIFT |
+            packet->type << TYPE_SHIFT |
+            packet->object << OBJECT_SHIFT |
+            temp;
+    memcpy(&serialized_packet[current_pos], &header, BASE_HEADER_BYTES);
+    current_pos += BASE_HEADER_BYTES;
+
     memcpy(&serialized_packet[current_pos], &packet->body, packet->size);
 
     return packet_size;
 }
 
+void deserialize_packet(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, uint8_t *received_packet, ssize_t rbytes)
+{
+    size_t current_pos = 0;
+    packet = dc_malloc(env, err, rbytes);
+    uint16_t size_size;
+    uint32_t header;
+
+}
+
 // send packet over TCP connection
-int send_packet(char* packet, char* server_ip, int server_port) {
+int send_packet(const struct dc_env *env, struct dc_error *err, struct base_packet *packet, char* server_ip, int server_port) {
+    uint8_t *serialized_packet;
+    uint8_t *received_packet;
+    size_t packet_size;
+    ssize_t wbytes;
+    ssize_t rbytes;
+    struct base_packet *deserialized_packet;
+
     int sock = socket(AF_INET, SOCK_STREAM, 0); // Create TCP socket
     if (sock == -1) {
         perror("Error: failed to create socket");
@@ -177,13 +204,40 @@ int send_packet(char* packet, char* server_ip, int server_port) {
         return -1;
     }
 
-    if (send(sock, packet, strlen(packet), 0) < 0) { // Send packet
-        perror("Error: failed to send packet");
-        return -1;
+    serialized_packet = NULL;
+    received_packet = NULL;
+    packet_size = serialize_packet(env, err, packet, serialized_packet);
+
+    wbytes = write_fully(sock, serialized_packet, packet_size);
+    if((size_t) wbytes != packet_size)
+    {
+        perror("wbytes != packet_size\n");
     }
 
-    //TODO: receive packet
+    //TODO: read into a 32bit integer, do the shifts, read the next however many bytes specified by size
 
     close(sock);
     return 0;
 }
+
+
+ssize_t write_fully(int fd, const void *buffer, size_t len)
+{
+    ssize_t total_bytes_write = 0;
+
+    while (total_bytes_write < (ssize_t)len)
+    {
+        ssize_t bytes_write = write(fd, (const uint8_t *)buffer + total_bytes_write, len - total_bytes_write);
+
+        if(bytes_write == -1)
+        {
+            perror("write");
+            exit(EXIT_FAILURE); // NOLINT(concurrency-mt-unsafe)
+        }
+
+        total_bytes_write += bytes_write;
+    }
+
+    return total_bytes_write;
+}
+
