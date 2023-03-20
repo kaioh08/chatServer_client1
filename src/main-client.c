521e28f85d7566b6fd5b8a052910018aa4eb6499
Kai <kaka8651@gmail.com>
2023-03-19 18:39:44 -0700

    send functions

--- src/main-client.c
+++ src/main-client.c
@@ -5,13 +5,66 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <dc_c/dc_stdio.h>
+#include <dc_c/dc_stdlib.h>
+#include <dc_c/dc_string.h>
+#include <dc_env/env.h>
+#include <dc_error/error.h>
+#include <dc_posix/dc_unistd.h>
+#include <netinet/in.h>
+
 
 #define SERVER_PORT 4981
 #define BUF_SIZE 256
+#define DEFAULT_SIZE 1024
+#define DEFAULT_VERSION 0x1
+
+enum Type {
+    CREATE = 0x1,
+    READ = 0x2,
+    UPDATE = 0x3,
+    DESTROY = 0x4,
+    PING = 0x8
+};
+
+enum Object {
+    USER = 0x01,
+    CHANNEL = 0x02,
+    MESSAGE = 0x03,
+    AUTH = 0x04
+};
+
+struct binary_header_field {
+    unsigned int version : 4; // 4 bit version number
+    unsigned int type : 4; // 4 bit type number
+    uint8_t object; // 8 bit object type
+    uint16_t body_size; // 16 bit body size
+};
 
 
 ssize_t write_fully(int fd, const void *buffer, size_t len);
 ssize_t read_fully(int fd, void *buffer, size_t len);
+void display_header(struct binary_header_field * header, const char * data);
+struct binary_header_field * deserialize_header(uint32_t value);
+void serialize_header(struct dc_env *env, struct dc_error *err, struct binary_header_field * header, int fd,
+                      const char * body);
+
+void send_create_user(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_create_channel(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_create_message(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_create_auth(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_read_user(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_read_channel(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_read_message(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_update_user(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_update_channel(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_update_message(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_update_auth(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_delete_user(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_delete_channel(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_delete_message(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+void send_delete_auth(struct dc_env *env, struct dc_error *err, int fd, const char * body);
+
 
 
 int main(int argc, char *argv[])
@@ -104,4 +157,107 @@ ssize_t read_fully(int fd, void *buffer, size_t len)
     }
 
     return total_bytes_read;
+}
+void send_create_user(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = CREATE;
+    header.object = USER;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
+}
+
+
+// send login (create auth) request
+void send_create_auth(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = CREATE;
+    header.object = AUTH;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
+}
+
+//Send Read user request
+void send_read_user(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = READ;
+    header.object = USER;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
+}
+
+//Send Update Stuff
+void send_update_user(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = UPDATE;
+    header.object = USER;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
+}
+
+void send_update_auth(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = CREATE;
+    header.object = AUTH;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
+}
+
+//Send delete user request
+void send_delete_user(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = DESTROY;
+    header.object = USER;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
+}
+
+//Send log out(delete auth)  user request
+void send_delete_auth(struct dc_env *env, struct dc_error *err, int fd, const char * body) {
+    DC_TRACE(env);
+
+    // Create header
+    struct binary_header_field header;
+    header.version = DEFAULT_VERSION;
+    header.type = DESTROY;
+    header.object = AUTH;
+    header.body_size = dc_strlen(env, body);
+    // Display and send the header
+    display_header(&header, body);
+    serialize_header(env, err, &header, fd, body);
 }
