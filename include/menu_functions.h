10b09da81f47f03c134d6b65d9517452676cc1cb
vasshorin <)tSz%gBWz1>
2023-04-14 13:58:53 -0700

    Add comments

--- include/menu_functions.h
+++ include/menu_functions.h
@@ -15,11 +15,50 @@ struct arg {
     int socket_fd;
 };
 
-
+/**
+ * Draws create new chat window, takes user input channel name, publicity, and sends it to server.
+ *
+ * @param env dc_env
+ * @param err dc_error
+ * @param socket_fd socket file descriptor
+ */
 void create_new_chat(struct dc_env *env, struct dc_error *err, int socket_fd);
+
+/**
+ * Draws active user list windows.
+ *
+ * @param env dc_env
+ * @param err dc_error
+ * @param socket_fd socket file descriptor
+ */
 void show_active_users(struct dc_env *env, struct dc_error *err, int socket_fd);
+
+/**
+ * Draws active chat list windows.
+ *
+ * @param env dc_env
+ * @param err dc_error
+ * @param socket_fd socket file descriptor
+ */
 void show_active_chats(struct dc_env *env, struct dc_error *err, int socket_fd);
+
+/**
+ * Draws user settings window.
+ *
+ * @param env dc_env
+ * @param err dc_error
+ * @param socket_fd socket file descriptor
+ */
 void display_settings(struct dc_env *env, struct dc_error *err, int socket_fd );
+
+/**
+ * Handles menu selection, calls appropriate function.
+ *
+ * @param env dc_env
+ * @param err dc_err
+ * @param socket_fd socket file descriptor
+ * @param choice menu option
+ */
 void handle_menu_selection(struct dc_env *env, struct dc_error *err, int socket_fd, int choice);
 
 #endif //PROCESS_SERVER_MENU_FUNCTIONS_H
